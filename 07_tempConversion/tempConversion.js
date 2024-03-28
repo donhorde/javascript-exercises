b30fba484a9bfad909021a3c5591a5a74d4f1ab3
Don <don.horde@gmail.com>
2024-03-28 20:41:09 +0100

    add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  return Math.ceil(((value-32)*5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return Math.ceil(((9/5 * value)+32)*10)/10;
 };
 
 // Do not edit below this line
