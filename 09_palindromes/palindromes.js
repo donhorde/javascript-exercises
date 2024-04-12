d72341e6759d2b97b599238f1202d7239c4ad696
Don <don.horde@gmail.com>
2024-04-12 21:57:03 +0200

    add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
 
+
+const palindromes = function (string) {
+    let lowercase = string.toLowerCase();
+    const pattern = /\w/g;
+    let lettersArray = lowercase.match(pattern);
+    let reversed = lettersArray.slice().reverse().join('');
+    let original = lettersArray.join('');
+    return (original === reversed);
 };
 
 // Do not edit below this line
