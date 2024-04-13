b7a16b921bc1795703e94bdabc2b296b1ab9777d
Don <don.horde@gmail.com>
2024-04-13 19:44:09 +0200

    add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+ let position = Number(input);
+ if (position == 0) {
+    return 0;
+ } else if (position < 1 || position !== position) {
+    return 'OOPS';
+ } else {
+    let first = 0;
+    let second = 1;
+    let interim = 0;
+    for (let i = 1; i < position; i++) {
+        interim = first+second;
+        first = second;
+        second = interim;
+    };
+    return second;
+ };
 };
 
 // Do not edit below this line
