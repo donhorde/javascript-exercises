888a15cc5725012a29b4cd8886e127cf7f2bbabc
Don <don.horde@gmail.com>
2024-04-13 20:47:37 +0200

    add findoldest solution using for of loop

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const d = new Date();
+    const currentYear = d.getFullYear();
+    let highestAge = 0;
+    let oldestPerson = {};
+    for (let person of array) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = currentYear;
+        };
+        if ((person.yearOfDeath - person.yearOfBirth) >= highestAge) {
+            highestAge = (person.yearOfDeath - person.yearOfBirth);
+            oldestPerson = person;
+        };
+    };
+    return oldestPerson;
 };
 
 // Do not edit below this line
